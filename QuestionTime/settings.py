44be5fb009ad17561b0bbb2373eda6ab14726a8d
hassan-boulhilt <hboulhilt@gmail.com>
2023-03-31 01:20:16 +0000

    Browser Authentication Workflow

--- QuestionTime/settings.py
+++ QuestionTime/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'QuestionTime.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,3 +130,10 @@ STATIC_URL = '/static/'
 
 # User model
 AUTH_USER_MODEL = 'users.CustomUser'
+
+
+# Account urls redirect
+
+LOGIN_URL = 'accounts/login'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
